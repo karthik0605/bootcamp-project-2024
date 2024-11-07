0ec605184132dcbac6f5afd34543b83f1ed5d587
Karthik Balaji <karthikb0606@gmail.com>
2024-11-07 00:31:30 -0800

    feat: Added styling to all main pages

--- website/src/app/contact/page.tsx
+++ website/src/app/contact/page.tsx
@@ -6,14 +6,22 @@ export default function Contact() {
   return (
     <header className={style.contact}>
       <h1 className="page-title">Contact</h1>
-      <form className="contact-form">
+      <form className={style.contactForm}>
         <label htmlFor="name">Name:</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required className={style.inputField} />
         <label htmlFor="email">Email:</label>
-        <input type="email" id="email" required />
-        <label htmlFor="name">Message:</label>
-        <textarea></textarea>
-        <input type="submit" value="Submit" />
+        <input type="email" id="email" required className={style.inputField} />
+        <label htmlFor="message">Message:</label>
+        <textarea
+          id="message"
+          required
+          className={style.textareaField}
+        ></textarea>
+        <div className={style.buttonGroup}>
+          <button type="submit" className={style.submitButton}>
+            Submit
+          </button>
+        </div>
       </form>
     </header>
   );
